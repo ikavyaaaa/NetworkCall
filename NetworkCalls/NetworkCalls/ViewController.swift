733167b3cf7b5d17ee4aa782d7ff091733e61103
Kavya Krishna <kavyakrishna@zktecodev.com>
2024-11-26 11:31:30 +0530

    Initial commit

---
+++ NetworkCalls/NetworkCalls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkCalls
+//
+//  Created by Kavya Krishna K. on 26/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

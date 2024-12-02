fc0694feb90317e454ed36ce46881842ee4add56
Kavya Krishna <kavyakrishna@zktecodev.com>
2024-12-02 16:28:33 +0530

    network call enums added

--- NetworkCalls/NetworkCalls/Resourses/Enum/Enum.swift
+++ NetworkCalls/NetworkCalls/Resourses/Enum/Enum.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+enum Encoding {
+    case JSON
+    case URL
+}
+
+enum HTTPHeaderField {
+    static let authentication  = "Authorization"
+    static let contentType     = "Content-Type"
+    static let acceptType      = "Accept"
+    static let acceptEncoding  = "Accept-Encoding"
+    static let acceptLangauge  = "accept-Language"
+    static let bearer          = "Bearer"
+    static let application     = "Application"
+    static let timezone        = "time-zone"
+    static let acceptLanguage  = "accept-language"
+    static let grantType       = "grant_type"
+}
+
+enum ContentType {
+    static let json            = "application/json"
+    static let multipart       = "multipart/form-data"
+    static let urlencoded      = "application/x-www-form-urlencoded"
+    static let ENUS            = "en-us"
+}
+
+enum MultipartType {
+    static let image = "Image"
+    static let csv = "CSV"
+}
+
+enum MimeType {
+    static let image = "image/png"
+    static let csvText = "text/csv"
+    static let formData = "application/x-www-form-urlencoded"
+}
